ce27ffc0e63ad6bf90180cf7822385f78bdb04e3
ming.yang <ming.yang@allpay.com.tw>
2014-07-03 13:11:30 +0800

    新增 顯示明細功能 修改 Model.js 將明細資料讀取獨立為Function

--- MVC3_CRUD_XML/Scripts/Billing/Billing.js
+++ MVC3_CRUD_XML/Scripts/Billing/Billing.js
@@ -2,23 +2,34 @@
 
 BillingApp.controller('BillingCtrl', function ($scope, $http, $window) {
 
-    $scope.IsShowEditPanel = true;
+    $scope.ShowMode = 'list';
 
     $http.get('/Billing/ListJobType').success(function (data) {
         $scope.JobTypes = data;
     });
 
-    $http.get('/Billing/ListBillings/').success(function (data) {
-        $scope.Billings = data;
-    });
+    $scope.InitData = function () {
+        $http.get('/Billing/ListBillings/').success(function (data) {
+            $scope.Billings = data;
+        });
+    }
 
     $scope.add = function () {
-        var billing = { "Customer": $scope.ICustomer, "JobType": $scope.IJobType, "Date": $scope.IDate, "Description": $scope.IDescription, "Hours": $scope.IHours };
+        var billing = { "Customer": $scope.ICustomer, "JobType": $scope.IJobType.id, "Date": $scope.IDate, "Description": $scope.IDescription, "Hours": $scope.IHours };
         $http.post('/Billing/Add/', billing).success(function (data) {
             if (data == "OK") {
                 $window.location.href = "/";
             }
         });
+    };
+
+    $scope.showDetail = function (Billing) {
+        $scope.BillingDetail = Billing;
+        $scope.ShowMode = 'details';
+    }
+
+    $scope.showList = function () {
+        $scope.ShowMode = 'list';
     };
 
     $scope.update = function () {
